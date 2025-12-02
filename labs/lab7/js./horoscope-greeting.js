b12c853a81dac08a9aadea02200bf4e0bcba7e68
korinnetoliver <korinne.toliver@uri.edu>
2025-12-02 14:46:41 -0500

    removed lab section

---
+++ labs/lab7/js./horoscope-greeting.js
@@ -0,0 +1,5 @@
+//console.log("Welcome to Cosmic Horoscopes! 🌟Discover your destiny today.")
+
+ //alert("Welcome to Cosmic Horoscopes! 🌟Discover your destiny today.")
+
+ document.write("Welcome to Cosmic Horoscopes! 🌟Discover your destiny today.")
